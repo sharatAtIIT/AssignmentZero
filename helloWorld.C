ce1205a71355dfad24fc9b25c9d4c51427790efc
sharat <sharat@cse.iitb.ac.in>
2018-01-17 14:56:26 +0530

    Starter code for hello world task

---
+++ helloWorld.C
@@ -0,0 +1,17 @@
+// Instructions:
+// use this starter code to write an OpenMP program that uses a function hello()
+// the function outputs 4 strings
+// These are your name, and the names of 3 other potential partners in
+// this course
+// Also the master thread outputs the honor code exactly once.
+// Obviously multiple workers must share the job
+
+#include <iostream>
+using namespace std;
+
+void hello();
+int main () {
+
+  cout << "Program Ends" << endl;  
+  return 0;
+}
