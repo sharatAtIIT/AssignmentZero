aa1e624efd2a5ed02bcc54cc82288b35e444779b
sharatAtIIT <35522104+sharatAtIIT@users.noreply.github.com>
2018-01-19 17:35:02 +0530

    Update helloWorld.C

--- helloWorld.C
+++ helloWorld.C
@@ -1,10 +1,12 @@
 // Instructions:
 // use this starter code to write an OpenMP program that uses a function hello()
-// the function outputs 4 strings
-// These are your name, and the names of 3 other potential partners in
-// this course
-// Also the master thread outputs the honor code exactly once.
-// Obviously multiple workers must share the job
+// the function outputs 4 strings. In parallel, obviously!
+// These are your name, and the names of 3 other names.. these are your potential partners in
+// this course for a course project
+// (Obviously multiple workers must share the job)
+//
+// Also the master thread outputs the honor code exactly once (so that's not going to be in hello()
+// 
 
 #include <iostream>
 using namespace std;
